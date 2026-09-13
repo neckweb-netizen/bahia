42efc7521a17c50c2051b92f7fe542e696d70dda
neckweb-netizen <neckweb@gmail.com>
2026-09-13 09:28:20 -0300

    fix: add service worker cache removal script

---
+++ sw-kill.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',async()=>{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));await self.registration.unregister();const clients=await self.clients.matchAll();clients.forEach(c=>c.postMessage({type:'SERVICE_WORKER_REMOVED'}));});
